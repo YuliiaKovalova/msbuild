d1d72fab826e6417fa71425bace511ea301b165f
YuliiaKovalova <ykovalova@microsoft.com>
2025-03-06 16:29:52 +0100

    cleanup TaskHostConfiguration

--- src/Shared/TaskHostConfiguration.cs
+++ src/Shared/TaskHostConfiguration.cs
@@ -104,117 +104,6 @@ namespace Microsoft.Build.BackEnd
 
 #if FEATURE_APPDOMAIN
         /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="nodeId">The ID of the node being configured.</param>
-        /// <param name="startupDirectory">The startup directory for the task being executed.</param>
-        /// <param name="buildProcessEnvironment">The set of environment variables to apply to the task execution process.</param>
-        /// <param name="culture">The culture of the thread that will execute the task.</param>
-        /// <param name="uiCulture">The UI culture of the thread that will execute the task.</param>
-        /// <param name="appDomainSetup">The AppDomainSetup that may be used to pass information to an AppDomainIsolated task.</param>
-        /// <param name="lineNumberOfTask">The line number of the location from which this task was invoked.</param>
-        /// <param name="columnNumberOfTask">The column number of the location from which this task was invoked.</param>
-        /// <param name="projectFileOfTask">The project file from which this task was invoked.</param>
-        /// <param name="continueOnError">Flag to continue with the build after a the task failed</param>
-        /// <param name="taskName">Name of the task.</param>
-        /// <param name="taskLocation">Location of the assembly the task is to be loaded from.</param>
-        /// <param name="isTaskInputLoggingEnabled">Whether task inputs are logged.</param>
-        /// <param name="taskParameters">Parameters to apply to the task.</param>
-        /// <param name="globalParameters">global properties for the current project.</param>
-        /// <param name="warningsAsErrors">Warning codes to be treated as errors for the current project.</param>
-        /// <param name="warningsNotAsErrors">Warning codes not to be treated as errors for the current project.</param>
-        /// <param name="warningsAsMessages">Warning codes to be treated as messages for the current project.</param>
-#else
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="nodeId">The ID of the node being configured.</param>
-        /// <param name="startupDirectory">The startup directory for the task being executed.</param>
-        /// <param name="buildProcessEnvironment">The set of environment variables to apply to the task execution process.</param>
-        /// <param name="culture">The culture of the thread that will execute the task.</param>
-        /// <param name="uiCulture">The UI culture of the thread that will execute the task.</param>
-        /// <param name="lineNumberOfTask">The line number of the location from which this task was invoked.</param>
-        /// <param name="columnNumberOfTask">The column number of the location from which this task was invoked.</param>
-        /// <param name="projectFileOfTask">The project file from which this task was invoked.</param>
-        /// <param name="continueOnError">Flag to continue with the build after a the task failed</param>
-        /// <param name="taskName">Name of the task.</param>
-        /// <param name="taskLocation">Location of the assembly the task is to be loaded from.</param>
-        /// <param name="isTaskInputLoggingEnabled">Whether task inputs are logged.</param>
-        /// <param name="taskParameters">Parameters to apply to the task.</param>
-        /// <param name="globalParameters">global properties for the current project.</param>
-        /// <param name="warningsAsErrors">Warning codes to be logged as errors for the current project.</param>
-        /// <param name="warningsNotAsErrors">Warning codes not to be treated as errors for the current project.</param>
-        /// <param name="warningsAsMessages">Warning codes to be treated as messages for the current project.</param>
-#endif
-        public TaskHostConfiguration(
-                int nodeId,
-                string startupDirectory,
-                IDictionary<string, string> buildProcessEnvironment,
-                CultureInfo culture,
-                CultureInfo uiCulture,
-#if FEATURE_APPDOMAIN
-                AppDomainSetup appDomainSetup,
-#endif
-                int lineNumberOfTask,
-                int columnNumberOfTask,
-                string projectFileOfTask,
-                bool continueOnError,
-                string taskName,
-                string taskLocation,
-                bool isTaskInputLoggingEnabled,
-                IDictionary<string, object> taskParameters,
-                Dictionary<string, string> globalParameters,
-                ICollection<string> warningsAsErrors,
-                ICollection<string> warningsNotAsErrors,
-                ICollection<string> warningsAsMessages)
-        {
-            ErrorUtilities.VerifyThrowInternalLength(taskName, nameof(taskName));
-            ErrorUtilities.VerifyThrowInternalLength(taskLocation, nameof(taskLocation));
-
-            _nodeId = nodeId;
-            _startupDirectory = startupDirectory;
-
-            if (buildProcessEnvironment != null)
-            {
-                _buildProcessEnvironment = buildProcessEnvironment as Dictionary<string, string>;
-
-                if (_buildProcessEnvironment == null)
-                {
-                    _buildProcessEnvironment = new Dictionary<string, string>(buildProcessEnvironment);
-                }
-            }
-
-            _culture = culture;
-            _uiCulture = uiCulture;
-#if FEATURE_APPDOMAIN
-            _appDomainSetup = appDomainSetup;
-#endif
-            _lineNumberOfTask = lineNumberOfTask;
-            _columnNumberOfTask = columnNumberOfTask;
-            _projectFileOfTask = projectFileOfTask;
-            _continueOnError = continueOnError;
-            _taskName = taskName;
-            _taskLocation = taskLocation;
-            _isTaskInputLoggingEnabled = isTaskInputLoggingEnabled;
-            _warningsAsErrors = warningsAsErrors;
-            _warningsNotAsErrors = warningsNotAsErrors;
-            _warningsAsMessages = warningsAsMessages;
-
-            if (taskParameters != null)
-            {
-                _taskParameters = new Dictionary<string, TaskParameter>(StringComparer.OrdinalIgnoreCase);
-
-                foreach (KeyValuePair<string, object> parameter in taskParameters)
-                {
-                    _taskParameters[parameter.Key] = new TaskParameter(parameter.Value);
-                }
-            }
-
-            _globalParameters = globalParameters ?? new Dictionary<string, string>();
-        }
-
-#if FEATURE_APPDOMAIN
-        /// <summary>
         /// Constructor.
         /// </summary>
         /// <param name="runtime">Task host runtime.</param>
@@ -281,25 +170,52 @@ namespace Microsoft.Build.BackEnd
                 ICollection<string> warningsAsErrors,
                 ICollection<string> warningsNotAsErrors,
                 ICollection<string> warningsAsMessages)
-            : this(nodeId,
-                  startupDirectory,
-                  buildProcessEnvironment,
-                  culture,
-                  uiCulture,
+        {
+            ErrorUtilities.VerifyThrowInternalLength(taskName, nameof(taskName));
+            ErrorUtilities.VerifyThrowInternalLength(taskLocation, nameof(taskLocation));
+
+            _nodeId = nodeId;
+            _startupDirectory = startupDirectory;
+
+            if (buildProcessEnvironment != null)
+            {
+                _buildProcessEnvironment = buildProcessEnvironment as Dictionary<string, string>;
+
+                if (_buildProcessEnvironment == null)
+                {
+                    _buildProcessEnvironment = new Dictionary<string, string>(buildProcessEnvironment);
+                }
+            }
+
+            _runtime = runtime;
+            _culture = culture;
+            _uiCulture = uiCulture;
 #if FEATURE_APPDOMAIN
-                appDomainSetup,
+            _appDomainSetup = appDomainSetup;
 #endif
-                  lineNumberOfTask,
-                  columnNumberOfTask,
-                  projectFileOfTask,
-                  continueOnError,
-                  taskName, taskLocation,
-                  isTaskInputLoggingEnabled,
-                  taskParameters,
-                  globalParameters,
-                  warningsAsErrors,
-                  warningsNotAsErrors,
-                  warningsAsMessages) => _runtime = runtime;
+            _lineNumberOfTask = lineNumberOfTask;
+            _columnNumberOfTask = columnNumberOfTask;
+            _projectFileOfTask = projectFileOfTask;
+            _continueOnError = continueOnError;
+            _taskName = taskName;
+            _taskLocation = taskLocation;
+            _isTaskInputLoggingEnabled = isTaskInputLoggingEnabled;
+            _warningsAsErrors = warningsAsErrors;
+            _warningsNotAsErrors = warningsNotAsErrors;
+            _warningsAsMessages = warningsAsMessages;
+
+            if (taskParameters != null)
+            {
+                _taskParameters = new Dictionary<string, TaskParameter>(StringComparer.OrdinalIgnoreCase);
+
+                foreach (KeyValuePair<string, object> parameter in taskParameters)
+                {
+                    _taskParameters[parameter.Key] = new TaskParameter(parameter.Value);
+                }
+            }
+
+            _globalParameters = globalParameters ?? new Dictionary<string, string>();
+        }
 
         /// <summary>
         /// Constructor for deserialization.
